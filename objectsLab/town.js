9d69ff4ae763228a1c16c7ef86a8205c80099169
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-05-26 23:24:58 +0300

    comp

---
+++ objectsLab/town.js
@@ -0,0 +1,24 @@
+function town (townList) {
+    const result = {};
+
+    for (const town of townList) {
+        const details = town.split(' <-> ')
+        const name = details[0]
+        const pop = details[1]
+
+        if (result[name] != undefined) {
+            pop += result[name]
+        }
+        result[name] = pop
+    } 
+
+    console.log(result)
+}
+
+town (['Istanbul <-> 100000',
+'Honk Kong <-> 2100004',
+'Jerusalem <-> 2352344',
+'Mexico City <-> 23401925',
+'Istanbul <-> 1000']
+
+)
