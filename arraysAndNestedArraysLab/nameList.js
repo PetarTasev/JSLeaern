9e08b8faa1d9c556757956eaab138e05a23da3d0
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-05-30 14:38:04 +0300

    wee

---
+++ arraysAndNestedArraysLab/nameList.js
@@ -0,0 +1,10 @@
+nameList = (array) => {
+    array.sort();
+    console.log(array)
+
+    for (let n = 1; n < array.length; n++) {
+        
+    }
+}
+nameList(["John", "Bob", "Christina", "Ema"]
+)
