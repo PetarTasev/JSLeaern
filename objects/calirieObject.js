b33ddc830cd60d6a9aca7a3c1b477971f860f35d
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-05-30 21:16:17 +0300

    yes

--- objects/calirieObject.js
+++ objects/calirieObject.js
@@ -1,3 +1,11 @@
-function calorieObject () {
-    
+function calorieObject (array) {
+    const object = {};
+    for (let n = 0; n < array.length; n += 2) {
+        let name = array[n];
+        let cal = Number(array[n + 1])
+        object[name] = (cal)
+    }
+    console.log(object)
 }
+
+calorieObject (['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
