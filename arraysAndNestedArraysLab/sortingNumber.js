56ee85e336272f85fbb183c862efda159e19da51
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-05-31 17:57:47 +0300

    ok

--- arraysAndNestedArraysLab/sortingNumber.js
+++ arraysAndNestedArraysLab/sortingNumber.js
@@ -9,3 +9,9 @@ sortingNumber ([1, 65, 3, 52, 48, 63, 31, -3, 18, 56])
 function sortNumber (array) {
 array 
 }
+
+// make a while cicly and just make a new array and push 
+// into it be using push and pop of the orrgininal array and 
+// and just switch them so you end up with a new array that 
+// consosts of the first and last element of the array just 
+// like that other exercise where we poped the unneeded numbers
