2ad2a15adccdffca55de1ac94b59fad372402bcb
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-06-08 00:37:05 +0300

    we up

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   const site = document.getElementById('content')
+
+   for (let section of words) {
+      const div = document.createElement('div');
+      const p = document.createElement('p')
+      p.textContent = section;
+      p.style.display = 'none'
+      div.appendChild(p)
+
+      div.addEventListener('click', show)
+      site.appendChild(div)
+
+      function show (ev) {
+         ev.currentTarget.children[0].style.display = ''
+      }
+   }
 }
