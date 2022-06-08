f6ba6437f7d472a690f06844c29dc3b2ca625971
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-06-08 15:23:53 +0300

    why it works but not on my browser ????

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+  Array.from(document.querySelectorAll(".profile button")).forEach((e) =>
+    e.addEventListener("click", opener)
+  );
+
+  function opener(ev) {
+    const parent = ev.target.parentElement;
+    const unlocked = parent.querySelector(
+      "input[type=radio][value=unlock]"
+    ).checked;
+
+    if (unlocked) {
+      let div = parent.querySelector('div');
+      console.log(div)
+      if (ev.target.textContent == "Show more") {
+        ev.target.textContent = "Hide it"
+        div.style.display = 'none'
+      } else {
+        ev.target.textContent = "Show more"
+        div.style.display = ''
+      }
+    }
+  }
 }
