6c37c819fed60914b2ac913db23b999febd7f87c
PetarTasev <petar.g.stefanov@gmail.com>
2022-06-11 19:56:46 +0300

    puah

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   let input = document.getElementById('searchText').value
+   let cityList = document.getElementsByTagName('li')
+   let total = 0;
+   let inputLength = input.length;
+
+   for (let n = 0; n < cityList.length; n++) {
+         cityList[n].style.textDecoration = 'none';
+         cityList[n].style.fontWeight = 'normal';
+      if (cityList[n].textContent.substring(0, inputLength) == input) {
+         //bold ad underline the thing;
+         cityList[n].style.textDecoration = 'underline';
+         cityList[n].style.fontWeight = 'bold';
+         total++
+      }
+   }
+   document.getElementById('result').textContent = `${total} matches found`
 }
