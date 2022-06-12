d75087c649ff329795311ffa1b482aef93ae5050
PetarTasev <petar.g.stefanov@gmail.com>
2022-06-12 17:26:17 +0300

    commit

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,18 +1,19 @@
 function search() {
-   let input = document.getElementById('searchText').value
-   let cityList = document.getElementsByTagName('li')
-   let total = 0;
-   let inputLength = input.length;
+  let input = document.getElementById("searchText").value;
+  let cityList = Array.from(document.getElementsByTagName("li"));
+  let total = 0;
 
-   for (let n = 0; n < cityList.length; n++) {
-         cityList[n].style.textDecoration = 'none';
-         cityList[n].style.fontWeight = 'normal';
-      if (cityList[n].textContent.substring(0, inputLength) == input) {
-         //bold ad underline the thing;
-         cityList[n].style.textDecoration = 'underline';
-         cityList[n].style.fontWeight = 'bold';
-         total++
-      }
-   }
-   document.getElementById('result').textContent = `${total} matches found`
+  for (let n = 0; n < cityList.length; n++) {
+    cityList[n].style.textDecoration = "";
+    cityList[n].style.fontWeight = "normal";
+  }
+
+  for (let n = 0; n < cityList.length; n++) {
+    if (cityList[n].textContent.match(input)) {
+      cityList[n].style.textDecoration = "underline";
+      cityList[n].style.fontWeight = "bold";
+      total++;
+    }
+  }
+  document.getElementById("result").textContent = `${total} matches found`;
 }
