abd2d39780db1ea934a90355a010b0223f4ba6fa
PetarTasev <petar.g.stefanov@gmail.com>
2022-06-12 22:26:57 +0300

    works

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   let input = document.getElementById('searchField')
+   const row = (document.querySelectorAll('tbody tr'))
+   
    function onClick() {
-      //   TODO:
+   
+      for (let kid of row) {
+         kid.classList.remove('select')
+         if (kid.innerHTML.includes(input.value)) {
+            kid.className = 'select'
+         }
+      }
+      input.value = ''
+      // go through every one and check enything matches 
+      // if yes class select ???
+
+      
 
    }
+
 }
