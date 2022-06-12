8e45b488d3b8c0dfdf79815437e8e0a3f00fae85
PetarTasev <petar.g.stefanov@gmail.com>
2022-06-12 17:34:49 +0300

    quickfix

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    function subtract() {
+        let num1 = document.getElementById('firstNumber').value
+        let num2 = document.getElementById('secondNumber').value
+    
+         let result = Number(num1) - Number(num2);
+    
+        let reslutElement = document.getElementById('result')
+        reslutElement.textContent = result
+    }
 }
