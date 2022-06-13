53412d7a7d6d39292647fee739c6697afc8d88a7
PetarTasev <petar.g.stefanov@gmail.com>
2022-06-13 20:31:07 +0300

    my teacher is so good at what he dose cant realte

--- 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+        const input = document.querySelectorAll('textarea')
+        const buttons = document.querySelectorAll('button')
+    
+        buttons[0].addEventListener('click', encode)
+        buttons[1].addEventListener('click', decode)
+
+        function encode () {
+            let encodedMessage = ''
+            let message = input[0].value
+            for (let n = 0; n < message.length; n++) {
+                encodedMessage += String.fromCharCode(message[n].charCodeAt(0) + 1)
+            }
+            input[1].value = encodedMessage
+            input[0].value = ''
+        
+        }
+        function decode () {
+            let decodedMessage = ''
+            let message = input[1].value
+            for (let n = 0; n < message.length; n++) {
+                decodedMessage += String.fromCharCode(message[n].charCodeAt(0) - 1)
+            }
+            input[1].value = decodedMessage
+        }
 }
