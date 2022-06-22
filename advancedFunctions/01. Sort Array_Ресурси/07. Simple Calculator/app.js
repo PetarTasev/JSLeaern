79afab33d138089a8a4d83c0c3ebd38d7832e144
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-06-22 14:29:24 +0300

    it finally works

--- advancedFunctions/01. Sort Array_Ресурси/07. Simple Calculator/app.js
+++ advancedFunctions/01. Sort Array_Ресурси/07. Simple Calculator/app.js
@@ -1,7 +1,21 @@
 function calculator() {
-    // TODO:
+    let number1 = null
+    let number2 = null
+    let result = null 
+    return {
+        init,
+        add,
+        subtract
+    }
+    function init (n1, n2, r) {
+        number1 = document.querySelector(n1)
+        number2 = document.querySelector(n2)
+        result = document.querySelector(r)
+    }
+    function add () {
+        result.value = Number(number2.value) + Number(number1.value)
+    }
+    function subtract () {
+        result.value = Number(number1.value) - Number(number2.value)
+    }
 }
-
-
-
-
