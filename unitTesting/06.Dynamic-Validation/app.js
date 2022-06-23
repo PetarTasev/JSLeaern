891674704f61d9f7e22eec40edcde2ca92304a3b
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-06-23 17:41:01 +0300

    commit

--- unitTesting/06.Dynamic-Validation/app.js
+++ unitTesting/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+  let email = document.getElementById("email");
+  let regex = /[a-z]+@[a-z]+.[a-z]+/g;
+  console.log('we it')
+
+  email.addEventListener("change", () => {
+    if (regex.test(email.value)) {
+        email.classList.remove('error')
+    } else {
+        email.classList.add('error')
+    }
+  });
 }
