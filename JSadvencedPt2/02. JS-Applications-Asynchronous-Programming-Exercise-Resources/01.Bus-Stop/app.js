fce383abcdd44f4b380eacc33773eec620b089ff
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-07-15 23:06:42 +0300

    comit

--- JSadvencedPt2/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JSadvencedPt2/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    console.log("TODO...");
+    const url ='http://localhost:3030/jsonstore/bus/businfo'
+    const input = document.getElementById('stopId')
+    const stopName = document.getElementById('stopName')
+    const buses = document.getElementById('buses')
+
+    fetch(`${url}/${input.value}`)
+        .then(response => response.json)
+        .then(data => {
+            let name = data.name
+            stopName.textContent = name
+
+            const bus = data.buses
+            buses.innerHTML = ''
+            Object.keys(bus).forEach(vihecul => {
+                const li = document.createElement('li')
+                li.textContent = `Bus ${vihecul} arrices in ${bus[vihecul]} minutes`
+                buses.appendChild(li)
+            })
+    })
+    .catch(error => {
+        stopName.textContent = 'Error'
+        buses.innerHTML = ''
+    })
 }
