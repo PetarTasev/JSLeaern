5f46566f99a4c8b4eca27d71827844a2ea72acf1
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-07-16 03:13:51 +0300

    it works prob

--- JSadvencedPt2/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ JSadvencedPt2/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,45 @@
 function solve() {
 
+    // when in motion only arrive is enabled 
+    // and the info box shows the upcoming stop
+
+
+    // when not in motion only deaprt in enabled
+    // and the info box shows the curent stop
+
+    //default
+    // info box is 'Not Connexted'
+    // arrive button is disabled
+    // id of thefirst stop is 'depot'
+
+    const infoBox = document.getElementById('info')
+    const url = 'http://localhost:3030/jsonstore/bus/schedule'
+    let curentStop = 'depot'
+    let stopName = ''
+
+    const departBtn = document.getElementById('depart')
+    const arriveBtn = document.getElementById('arrive')
+
     function depart() {
-        console.log('Depart TODO...');
+        departBtn.disabled = true
+        arriveBtn.disabled = false
+
+        fetch(`${url}/${curentStop}`)
+            .then(info => info.json())
+            .then(data => {
+                stopName = data.name
+                infoBox.textContent = `Next stop ${stopName}`
+                curentStop = data.next
+            })
+
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true
+        departBtn.disabled = false
+
+        infoBox.textContent = `Arriving at ${stopName}`
     }
 
     return {
