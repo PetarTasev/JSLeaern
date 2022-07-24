0ea58f1ec3f68ace1a74eb7ccf84c1f625e75241
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-07-25 02:07:55 +0300

    gettingthe hang of it

--- JSadvencedPt2/spa-demo-master/spa-demo-master/login.js
+++ JSadvencedPt2/spa-demo-master/spa-demo-master/login.js
@@ -1,6 +1,49 @@
+import { homeView } from "./home.js"
+import { checkUserNav } from "./check.js"
+
 const home = document.getElementById('loginView')
+const form = home.querySelector('form')
+form.addEventListener('submit', logIn)
 home.remove()
+
 
 export function logInView() {
     document.getElementById('views').replaceChildren(home)
+}
+
+async function  logIn(ev) {
+    ev.preventDefault()
+
+    const formData = new FormData(form)
+    const email = formData.get('email').trim()
+    const password = formData.get('password').trim()
+
+    try {
+        const res = await fetch('http://localhost:3030/users/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        }) 
+
+        if (res.ok == false) {
+            const error = await res.json()
+            throw Error(error.message)
+        }
+        const data = await res.json()
+        
+        const userData = {
+            email: data.email,
+            accessToken: data.accessToken,
+            id: data._id
+        }
+
+        sessionStorage.setItem('userData', JSON.stringify(userData))
+        checkUserNav()
+        homeView()
+
+    } catch(err) {
+        alert(err.message)
+    }
 }
--- JSadvencedPt2/spa-demo-master/spa-demo-master/register.js
+++ JSadvencedPt2/spa-demo-master/spa-demo-master/register.js
@@ -1,6 +1,13 @@
+
 const home = document.getElementById('registerView')
+const form = home.querySelector('form')
+form.addEventListener('submit', register)
 home.remove()
 
 export function registerView() {
     document.getElementById('views').replaceChildren(home)
+}
+
+function register(ev) {
+    ev.preventDefault()
 }
