ccf2c08c6629ad297075af9ae94a4fbdb22921ab
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-07-25 01:26:43 +0300

    commit

--- JSadvencedPt2/spa-demo-master/spa-demo-master/create.js
+++ JSadvencedPt2/spa-demo-master/spa-demo-master/create.js
@@ -1,10 +1,6 @@
-//const list = querySelector('ul') 
-const catalog = getElementById('catalogView')
-
+const catalog = document.getElementById('catalogView')
 
 export async function addMovie() {
-    const input = document.createElement('input')
-    input.classList('text')
-    catalog.appendChild(input)
+
     
 }
