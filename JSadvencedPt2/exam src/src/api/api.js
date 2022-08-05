d3b96bd4ad8d29cc2e460627236af0a4d4516c26
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-08-05 20:41:12 +0300

    commit

---
+++ JSadvencedPt2/exam src/src/api/api.js
@@ -0,0 +1,103 @@
+
+const host = 'http://localhost:3030'
+
+async function requeset (method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (data != undefined) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(data)
+    }
+
+     const userData = getUserData()
+    if (userData) {
+        options.headers['X-Authorization'] = userData.accessToken
+    }
+
+    try {
+        const res = await fetch(host + url, options)
+
+        if (res.ok == false) {
+            const error = await res.json()
+            throw new Error(error.message)
+        }
+
+        if (res.status == 204) {
+            return res
+        } else {
+            return res.json()
+        }
+
+    } catch (err) {
+        alert(err.message)
+        throw err
+    }
+}
+
+
+export async function get(url) {
+    return requeset('get', url)
+}
+
+export async function post(url, data) {
+    return requeset('post',url, data)
+}
+
+export async function put(url, data) {
+    return requeset('put',url, data)
+}
+
+
+export async function del(url) {
+    return requeset('delete', url)
+}
+
+
+
+
+
+export async function logIn(email, password) {
+    const result = await post('/users/login', {email, password})
+
+    let userData = {
+        email: result.email,
+        id: result._id,
+        accessToken: result.accessToken
+    }
+
+    setUserData(userData)
+    return result
+}
+
+export async function regester(email, password) {
+    let result = await post('/users/register', { email, password })
+
+    let userData = {
+        email: result.email,
+        id: result._id,
+        accessToken: result.accessToken
+    }
+
+    setUserData(userData)
+    return result
+}
+
+export async function logout() {
+     await get('/users/logout')
+    clearUserData()
+}
+
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem('userData'))
+}
+
+export function setUserData(data) {
+    sessionStorage.setItem('userData', JSON.stringify(data))
+}
+
+export function clearUserData() {
+    sessionStorage.removeItem('userData')
+}
---
+++ JSadvencedPt2/exam src/src/api/data.js
@@ -0,0 +1,6 @@
+import * as api from './api.js'
+
+export const login = api.logIn
+export const regester = api.regester
+export const logout = api.logout
+
