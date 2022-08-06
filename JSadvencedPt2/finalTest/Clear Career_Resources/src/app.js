3d95376dbd833cb1ba2b276bfbd08eb71c39d74c
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-08-06 09:07:06 +0300

    commmit

---
+++ JSadvencedPt2/finalTest/Clear Career_Resources/src/app.js
@@ -0,0 +1,45 @@
+import { render } from "../node_modules/lit-html/lit-html.js";
+import page from "../node_modules/page/page.mjs"
+import { logout } from "./api/api.js";
+import { getUserData } from "./util.js"
+import { loginPage } from "./views/loginPage.js";
+import { registerPage } from "./views/registerPage.js";
+
+
+let root = document.getElementById('site-content')
+
+const del = document.getElementById('logoutBtn')
+del.addEventListener('click', (e) => {
+    e.preventDefault()
+    logout()
+    updateUserNav()
+    page.redirect('/')
+})
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root)
+    ctx.updateUserNav = updateUserNav
+    next()
+}
+
+export function updateUserNav() {
+    const userData = getUserData()
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block'
+        document.getElementById('guest').style.display = 'none'
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`
+    } else {
+        document.getElementById('user').style.display = 'none'
+        document.getElementById('guest').style.display = 'inline-block'
+    }
+}
+
+
+page(decorateContext)
+
+page('/', '/home')
+page('/login', loginPage)
+page('/register', registerPage)
+updateUserNav()
+page.start()
+page.redirect('/')
